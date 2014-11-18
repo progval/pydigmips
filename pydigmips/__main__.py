19cbad8305f8e2f2477ca8f9a20f98a2686ba7e7
Valentin Lorentz <progval@progval.net>
2014-11-18 09:59:35 +0100

    Document CLI switches.

--- pydigmips/__main__.py
+++ pydigmips/__main__.py
@@ -21,13 +21,20 @@ parser.add_argument('hexfile',
         type=argparse.FileType('r'),
         help='The STDOUT output of the assembler')
 parser.add_argument('--infinite-loop', dest='infinite_loop',
-        action='store_true')
+        action='store_true',
+        help='Enable simple infinite loop detection (stops when '
+             'the emulator has been in the same state twice.)')
 parser.add_argument('--beq', dest='beq',
-        action='store_true')
+        action='store_true',
+        help='Use old instruction set, which implements BEQ instead of '
+             'BLE at opcode 4.')
 parser.add_argument('--trace-inst', dest='trace_inst',
-        action='store_true')
+        action='store_true',
+        help='Shows the Program Counter and the executed instruction '
+             'while running.')
 parser.add_argument('--trace-mem', dest='trace_mem',
-        action='store_true')
+        action='store_true',
+        help='Show the state of registers and data memory while running.')
 if __name__ == '__main__':
     args = parser.parse_args()
     main(args.hexfile, infinite_loop=args.infinite_loop, beq=args.beq,
