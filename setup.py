2dab402af3c7cba5a3164d9d8f2a23426e6ea107
Valentin Lorentz <progval@progval.net>
2015-02-13 16:07:55 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydigmips',
-    version='0.1.1',
+    version='0.2',
     description='DigMIPS interpreter.',
     url='https://github.com/ProjetPP/pydigmips',
     author='Valentin Lorentz',
